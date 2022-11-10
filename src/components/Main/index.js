ac728c27a11de7e509d286cc4606b2bd51a8f3cb
NicholasPervez <njpervez@gmail.com>
2022-11-10 17:01:43 +0000

    half of task 5 working

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,8 +8,38 @@ import Post from '../Post';
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
+  
+  return <main id="main">
 
-  return <main id="main"></main>;
-}
+
+  {posts.map(
+    (post) => { return <Post key={post.postId} 
+    title={post.title}
+    date={post.date}
+    author={post.author}
+    text={post.text}
+    highlights={post.highlights}
+    image={post.image}
+
+
+    ></Post>
+    })};
+
+    
+
+  </main>;
+};
 
 export default Main;
+
+// postId: 0,
+// title: 'a title',
+// date: 'DD/MM/YY',
+// author: 'author',
+// text: lorem.generateParagraphs(7),
+// highlights: [
+//   lorem.generateWords(3),
+//   lorem.generateWords(3),
+//   lorem.generateWords(3),
+// ],
+// image: { link: 'link', alt: 'alt' },
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({key, title, date, author, text, highlights, image}) {
+  return <article>
+    <h1>{title}</h1>
+    <h2>{date}</h2>
+    <h3>{author}</h3>
+    <p>{text}</p>
+    {highlights.map((currentElement, index)=> {
+      return <li key={index}>{currentElement}</li>
+    })}
+
+
+  </article>;
 }
 
 export default Post;
